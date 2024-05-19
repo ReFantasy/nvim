b8adc6868cf011a1f7f58446cf04c4cfb3414049
refantasy <refantasy@debian.refantasy.cn>
2024-05-19 21:12:00 +0800

    .

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -10,12 +10,29 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup{
-				--ensure_installed = { "pyright" },
+				--ensure_installed = { "lua_ls" },
 			}
+			require("lspconfig").clangd.setup{
+                enabled = true,
+                cmd =
+                {
+                    "clangd",
+                    "--background-index",
+                     "--clang-tidy",
+                     "--completion-style=detailed",
+                },
+            }
 
-			require("lspconfig").clangd.setup{}
-			require("lspconfig").pylsp.setup{}
-			
+			require("lspconfig").pylsp.setup{
+                settings = {
+                    pylsp = {
+                      plugins = {
+                        pycodestyle = {enabled = false},
+                        pyflakes = {enabled = true},
+                      }
+                    }
+                   }
+            }
 		end
 	},
 

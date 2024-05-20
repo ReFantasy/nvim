ce2beed6ec52c88455cf45ecf9d628ad78096f03
refantasy-debian <309741309@qq.com>
2024-05-20 21:27:47 +0800

    .

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,7 +1,10 @@
 return{
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.7',
-         dependencies = { 'nvim-lua/plenary.nvim'},
+         dependencies = { 
+             'nvim-lua/plenary.nvim',
+             {'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+         },
          config = function()
              local builtin = require('telescope.builtin')
              vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -19,7 +22,19 @@ return{
                      preview = true,
                      file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
                  },
+                 extensions = {
+                    fzf = {
+                      fuzzy = true,                    -- false will only do exact matching
+                      override_generic_sorter = true,  -- override the generic sorter
+                      override_file_sorter = true,     -- override the file sorter
+                      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                                       -- the default case_mode is "smart_case"
+                    }
+                 },
              }
+             -- To get fzf loaded and working with telescope, you need to call
+             -- load_extension, somewhere after setup function:
+             require('telescope').load_extension('fzf')
          end,
     },
     {

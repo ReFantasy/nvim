c3ba37ee295da7ad96250e22025c985a18cc8859
ReFantasy <309741309@qq.com>
2024-05-21 16:30:22 +0800

    add terminal plugin

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,3 +1,11 @@
 return {
-    {'akinsho/toggleterm.nvim', version = "*", config = true}
+        'akinsho/toggleterm.nvim', 
+         tag = "v2.11.0", 
+         config = function()
+             require("toggleterm").setup({
+                 open_mapping = [[<c-\>]],
+                 direction = "float"
+             })
+
+         end,
 }

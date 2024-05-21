6bd6b75cf721c37c1421769c23347540ce7517ee
refantasy-debian <309741309@qq.com>
2024-05-21 20:30:25 +0800

    update lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -20,6 +20,59 @@ function get_os_type()
             end
 end
 
+function get_gpu_usage()
+    local handle = io.popen("nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits")
+    local result = handle:read("*a")
+    handle:close()
+
+    if result then
+        local usage = tonumber(result:match("%d+"))
+        if usage then
+            --return usage .. "%"
+            --return "GPU:" .. tostring(usage)
+            return "GPU:" .. string.format("%3.0f", usage)
+        else
+            return "Unable to parse GPU usage"
+        end
+    else
+        return "Unable to get GPU usage"
+    end
+end
+
+function get_cpu_usage()
+    local os_type = get_os_type()
+    
+    if os_type == "Windows" then
+        -- Windows system
+        local handle = io.popen("wmic cpu get loadpercentage /value")
+        local result = handle:read("*a")
+        handle:close()
+
+        local usage = result:match("LoadPercentage=(%d+)")
+        if usage then
+            return usage .. "%"
+        else
+            return "Unable to get CPU usage"
+        end
+    else
+        -- Unix-like system
+        local handle = io.popen("top -bn1 | grep 'Cpu(s)'")
+        local result = handle:read("*a")
+        handle:close()
+
+        local usage = result:match("(%d+%.%d+) id")
+        if usage then
+            usage = 100 - tonumber(usage)
+            return "CPU:" .. usage --string.format("%.2f%%", usage)
+        else
+            return "Unable to get CPU usage"
+        end
+    end
+end
+
+
+
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -35,13 +88,16 @@ return {
 		    },
             
 			sections = {
-			    lualine_a = {'mode',get_os_type},
+			    lualine_a = {'mode', get_os_type },
 			    lualine_b = {'branch', 'diff', 'diagnostics'},
 			    lualine_c = {'filename'},
-			    lualine_x = {'encoding' , 'hostname'},
+			    lualine_x = {'encoding' , get_gpu_usage},
 			    lualine_y = {'progress'},
 			    lualine_z = {'location'}
 		    },
+           refresh = {
+               statusline = 1000,
+           },
 	    }
     end,
 }

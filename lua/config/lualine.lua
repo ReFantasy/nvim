7c1c5dda722373f0ea62bcc428816632ebd5ce9e
refantasy-debian <309741309@qq.com>
2024-05-21 19:39:41 +0800

    update lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,11 +1,37 @@
+function get_os_type()
+            local separator = package.config:sub(1,1)
+    
+            if separator == '\\' then
+                return "Windows"
+            else
+            -- For Unix-like systems
+                local handle = io.popen("uname -a")
+                local result = handle:read("*a")
+                handle:close()
+                if result:find("Debian") then
+                    return require'nvim-web-devicons'.get_icon('Debian', '', options)
+                elseif result:find("Ubuntu") then
+                    return require'nvim-web-devicons'.get_icon('ubuntu', '', options)
+                elseif result:find("Darwin") then
+                    return require'nvim-web-devicons'.get_icon('apple', '', options)
+                else
+                    return require'nvim-web-devicons'.get_icon('linux', '', options)
+
+                end
+            end
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
 	    local custom_theme = require'lualine.themes.onedark'
-            local function hello()
-		    return tostring(cpustat)
-	    end
+        
+        local function window()
+                --return vim.api.nvim_win_get_number(0)
+                --return require'nvim-web-devicons'.get_icon('apple', '', options)
+                return os.getenv("OS")
+        end
 
 	    require("lualine").setup{
 		    -- options = {tneme='vscode'}
@@ -15,9 +41,9 @@ return {
 			    section_separators = { left = '', right = ''},
 			    component_separators = { left = '|', right = '|' }
 		    },
-
-			    sections = {
-			    lualine_a = {'mode'},
+            
+			sections = {
+			    lualine_a = {'mode',get_os_type},
 			    lualine_b = {'branch', 'diff', 'diagnostics'},
 			    lualine_c = {'filename'},
 			    lualine_x = {'encoding' , 'hostname'},

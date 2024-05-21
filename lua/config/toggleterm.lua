cbf55e6a0f3d039e754380ab6671d73c592dc85a
ReFantasy <309741309@qq.com>
2024-05-21 16:59:34 +0800

    update terminal

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -4,8 +4,8 @@ return {
          config = function()
              require("toggleterm").setup({
                  open_mapping = [[<c-\>]],
-                 direction = "float"
+                 direction = "float",
+                 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
              })
-
          end,
 }

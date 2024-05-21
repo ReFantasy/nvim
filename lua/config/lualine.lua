55c906af5a7f8485c2c6f429d65166572d44631c
refantasy-debian <309741309@qq.com>
2024-05-21 19:44:29 +0800

    update lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -2,21 +2,20 @@ function get_os_type()
             local separator = package.config:sub(1,1)
     
             if separator == '\\' then
-                return "Windows"
+                return require'nvim-web-devicons'.get_icon('windows', '')
             else
             -- For Unix-like systems
                 local handle = io.popen("uname -a")
                 local result = handle:read("*a")
                 handle:close()
                 if result:find("Debian") then
-                    return require'nvim-web-devicons'.get_icon('Debian', '', options)
+                    return require'nvim-web-devicons'.get_icon('Debian', '')
                 elseif result:find("Ubuntu") then
-                    return require'nvim-web-devicons'.get_icon('ubuntu', '', options)
+                    return require'nvim-web-devicons'.get_icon('ubuntu', '')
                 elseif result:find("Darwin") then
-                    return require'nvim-web-devicons'.get_icon('apple', '', options)
+                    return require'nvim-web-devicons'.get_icon('apple', '')
                 else
-                    return require'nvim-web-devicons'.get_icon('linux', '', options)
-
+                    return require'nvim-web-devicons'.get_icon('linux', '')
                 end
             end
 end
@@ -27,14 +26,7 @@ return {
     config = function()
 	    local custom_theme = require'lualine.themes.onedark'
         
-        local function window()
-                --return vim.api.nvim_win_get_number(0)
-                --return require'nvim-web-devicons'.get_icon('apple', '', options)
-                return os.getenv("OS")
-        end
-
 	    require("lualine").setup{
-		    -- options = {tneme='vscode'}
 		    options = {
 			    theme = custom_theme,
 			    icons_enabled = false,
@@ -43,7 +35,7 @@ return {
 		    },
             
 			sections = {
-			    lualine_a = {'mode',get_os_type},
+			    lualine_a = {get_os_type,'mode'},
 			    lualine_b = {'branch', 'diff', 'diagnostics'},
 			    lualine_c = {'filename'},
 			    lualine_x = {'encoding' , 'hostname'},

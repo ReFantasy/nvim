978a4e1189a6417313f78e8c3bb4b106e3c93d55
refantasy-debian <309741309@qq.com>
2024-05-22 10:37:58 +0800

    update lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -4,7 +4,6 @@ function get_os_icon()
             if separator == '\\' then
                 return require'nvim-web-devicons'.get_icon('windows', '')
             else
-            -- For Unix-like systems
                 local handle = io.popen("uname -a")
                 local result = handle:read("*a")
                 handle:close()
@@ -20,93 +19,106 @@ function get_os_icon()
             end
 end
 
-
-
-function get_gpu_usage()
-    local handle = io.popen("nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits")
-    local result = handle:read("*a")
-    handle:close()
-
-    if result then
-        local usage = tonumber(result:match("%d+"))
-        if usage then
-            --return usage .. "%"
-            --return "GPU:" .. tostring(usage)
-            return "GPU:" .. string.format("%3.0f", usage)
-        else
-            return nil --"Unable to parse GPU usage"
-        end
-    else
-        return nil--"Unable to get GPU usage"
-    end
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
 end
 
-function get_cpu_usage()
-    local os_type = get_os_type()
-    
-    if os_type == "Windows" then
-        -- Windows system
-        local handle = io.popen("wmic cpu get loadpercentage /value")
-        local result = handle:read("*a")
-        handle:close()
+local mode = {
+	"mode",
+	--fmt = function(str)
+	--	return "-- " .. str .. " --"
+	--end,
+}
 
-        local usage = result:match("LoadPercentage=(%d+)")
-        if usage then
-            return usage .. "%"
-        else
-            return "Unable to get CPU usage"
-        end
-    else
-        -- Unix-like system
-        local handle = io.popen("top -bn1 | grep 'Cpu(s)'")
-        local result = handle:read("*a")
-        handle:close()
+local branch = {
+	"branch",
+    icons_enabled = false,
+}
 
-        local usage = result:match("(%d+%.%d+) id")
-        if usage then
-            usage = 100 - tonumber(usage)
-            return "CPU:" .. usage --string.format("%.2f%%", usage)
-        else
-            return "Unable to get CPU usage"
-        end
-    end
-end
+local diff = {
+	"diff",
+	--colored = false,
+	--symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    --cond = hide_in_width
+}
+
+local diagnostics = {
+	"diagnostics",
+	--sources = { "nvim_diagnostic" },
+	--sections = { "error", "warn" },
+	--symbols = { error = " ", warn = " " },
+	--colored = false,
+	--update_in_insert = false,
+	--always_visible = true,
+}
 
 
 
+local filename= {
+	"filename",
+}
+
+local hostname= {
+	"hostname",
+    color = {fg='#fd567c'},
+}
+
+local location = {
+	"location",
+	padding = 0,
+}
+
+-- cool function for progress
+local progress = {
+    "progress"
+}
 
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-	    local custom_theme = require'lualine.themes.onedark'
-        --custom_theme.normal.b.bg = '#cc7979'
 
-        function git_icon()
-            return require'nvim-web-devicons'.get_icon('git', '')
+        local status_ok, lualine = pcall(require, "lualine")
+        if not status_ok then
+            return
         end
-       
-	    require("lualine").setup{
-		    options = {
-			    theme = custom_theme,
-			    icons_enabled = false,
+
+	    local custom_theme = require'lualine.themes.onedark'
+
+        lualine.setup({
+            options = {
+                theme = custom_theme, --"auto",
+                icons_enabled = false,
 			    section_separators = { left = '', right = ''},
-			    component_separators = { left = '|', right = '|' }
-		    },
-            
-			sections = {
-			    lualine_a = {'mode', get_os_icon},
-			    lualine_b = {{'branch', icons_enabled=false}, 'diff', 'diagnostics'},
-			    lualine_c = {'filename'},
-			    lualine_x = {
-                    {'hostname', color = {fg='#fd567c'}},
-                },
-			    lualine_y = {'progress' },
-			    lualine_z = {'location'}
-		    },
-           refresh = {
-               statusline = 1000,
-           },
-	    }
-    end,
+			    component_separators = { left = '|', right = '|' },
+                disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+                always_divide_middle = true,
+
+            },
+            sections = {
+                lualine_a = { mode, get_os_icon},
+                lualine_b = { branch, diff, diagnostics},
+                lualine_c = { filename },
+                lualine_x = { hostname },
+                lualine_y = { progress },
+                lualine_z = { location },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            extensions = {},
+        })
+
+    end
 }
+
+
+
+
+

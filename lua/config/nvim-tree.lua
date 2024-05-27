11b5a37cae2457af728bc0aa7e4616233622ad17
ReFantasy <309741309@qq.com>
2024-05-27 14:27:19 +0800

    .

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function nvtree_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  -- vim.keymap.set('n', '<C-u>', api.tree.change_root_to_parent,        opts('Up'))
+  -- vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
@@ -10,10 +25,11 @@ return {
 		config = function()
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
+
+            vim.api.nvim_set_keymap("n", "<A-t>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
             
 			require("nvim-tree").setup({
-                renderer = {group_empty = true,},
-                filters = {dotfiles = true},
+                on_attach = nvtree_on_attach
             })
 		end,
 	}

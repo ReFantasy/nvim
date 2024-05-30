c88cbb142c9a567fe0b005087b1f915357120942
ReFantasy <309741309@qq.com>
2024-05-30 12:35:20 +0800

    .

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -16,6 +16,7 @@ return {
              
 
              -- python
+             --[[
              dap.adapters.python = {
                  type = 'executable';
                  command = 'python';
@@ -32,6 +33,7 @@ return {
                  end;
                },
               }
+              ]]--
 
 		 end,
     },
@@ -40,6 +42,14 @@ return {
         'theHamsta/nvim-dap-virtual-text',
         config = function()
             require('nvim-dap-virtual-text').setup()
+        end,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {"williamboman/mason.nvim",}, 
+        config = function()
+            require ('mason-nvim-dap').setup({
+            })
         end,
     },
 }

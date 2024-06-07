1947981af6f8611e723378f222bf902d1310e3d6
ReFantasy <309741309@qq.com>
2024-06-07 16:12:39 +0800

    update

--- init.lua
+++ init.lua
@@ -9,8 +9,8 @@ vim.keymap.set('n', '<F4>', cmake_build.build)
 -- define your colorscheme here
 -- vim.cmd("colorscheme monokai_pro")
 --local colorscheme = 'embark'
---local colorscheme = 'tokyonight-moon'
---local colorscheme = 'vscode'
+-- local colorscheme = 'tokyonight-moon'
+-- local colorscheme = 'vscode'
 local colorscheme = 'monokai_pro'
 local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not is_ok then
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -51,4 +51,52 @@ return {
             }
         end
     },
+    {
+        -- 快速跳转
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        's1n7ax/nvim-window-picker',
+        name = 'window-picker',
+        event = 'VeryLazy',
+        version = '2.*',
+        config = function()
+            require 'window-picker'.setup({
+                filter_rules = {
+                    -- whether you want to include the window you are currently on to window
+                    -- selection or not
+                    include_current_win = true,
+
+                    -- filter using buffer options
+                    bo = {
+                        -- if the file type is one of following, the window will be ignored
+                        filetype = { 'NvimTree', 'neo-tree', 'notify' },
+
+                        -- if the file type is one of following, the window will be ignored
+                        buftype = { 'terminal' },
+                    },
+                },
+            })
+
+            -- 选择窗口函数
+            local pick_win = function()
+                local window_number = require('window-picker').pick_window()
+                if window_number then vim.api.nvim_set_current_win(window_number) end
+            end
+
+            vim.keymap.set('n', '<leader>pw', pick_win, { desc = '[C] Pick Window' })
+        end,
+
+    },
 }

bb147115f9f610e188a35eb6378b896b7230744b
refantasy <refantasy@debian.debian.refantasy.cn>
2024-06-21 09:07:52 +0800

    .

--- lua/plugins/extra_telescope.lua
+++ lua/plugins/extra_telescope.lua
@@ -1,10 +1,11 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.7',
+        tag = '0.1.6',
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+            -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
         config = function()
             local status, actions = pcall(require, "telescope.actions")

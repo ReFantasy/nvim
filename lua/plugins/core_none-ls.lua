10ab47cf18aea743cc494a9acd90eb77345a85ec
ReFantasy <309741309@qq.com>
2024-06-27 20:58:04 +0800

    update format config

--- lua/plugins/core_none-ls.lua
+++ lua/plugins/core_none-ls.lua
@@ -1,19 +1,20 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        require("null-ls").setup({
-            sources = {
-                null_ls.builtins.formatting.clang_format.with({
-                    extra_args = { "--style", "Microsoft" },
-                }),
-                -- null_ls.builtins.diagnostics.pylint,
-                null_ls.builtins.formatting.black,
-                -- null_ls.builtins.diagnostics.selene,
-            },
-        })
-    end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua.with({}),
+				null_ls.builtins.formatting.black.with({
+					extra_args = { "--line-length", 128 },
+				}),
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = { "--style", "Microsoft" },
+				}),
+			},
+		})
+	end,
 }

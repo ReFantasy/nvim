38a1e1ad31488aa0f6160313d86a61a0ee788996
ReFantasy <309741309@qq.com>
2024-06-27 22:08:15 +0800

    .

--- lua/plugins/extra_file_tree.lua
+++ lua/plugins/extra_file_tree.lua
@@ -1,167 +1,150 @@
--- local function my_on_attach(bufnr)
---     local api = require "nvim-tree.api"
---
---     local function opts(desc)
---         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---     end
---
---     -- default mappings
---     api.config.mappings.default_on_attach(bufnr)
---
---     -- custom mappings
---     -- vim.keymap.set('n', '<C-u>', api.tree.change_root_to_parent,        opts('Up'))
---     vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
--- end
---
--- return {
---     {
---         "nvim-tree/nvim-tree.lua",
---         enabled = true,
---         version = "*",
---         lazy = false,
---         dependencies = {
---             "nvim-tree/nvim-web-devicons",
---         },
---
---         config = function()
---             vim.g.loaded_netrw = 1
---             vim.g.loaded_netrwPlugin = 1
---
---             local glyphs = {}
---             local installed, circles = pcall(require, 'circles')
---             if installed then
---                 circles.setup({ icons = { empty = '', filled = '', lsp_prefix = '' } })
---                 glyphs = circles.get_nvimtree_glyphs()
---             end
---             glyphs.git = {
---                 unstaged = '',
---                 staged = '',
---                 unmerged = '',
---                 renamed = '',
---                 untracked = '',
---                 deleted = '',
---                 ignored = '',
---             }
---
---             require("nvim-tree").setup({
---                 on_attach = my_on_attach,
---                 filters = {
---                     dotfiles = true,
---                 },
---                 diagnostics = { enable = true },
---                 view = {
---                     width = 30,
---                     side = 'left',
---                     signcolumn = 'no',
---                 },
---                 git = {
---                     enable = true,
---                     ignore = false,
---                     timeout = 500,
---                 },
---                 actions = {
---                     open_file = {
---                         -- quit_on_open = true,
---                     },
---                 },
---                 renderer = {
---                     group_empty = true,
---                     highlight_opened_files = 'all',
---                     special_files = {},
---                     root_folder_modifier = ':p:~',
---                     icons = {
---                         glyphs = glyphs,
---                     },
---                 },
---             })
---
---             local api = require "nvim-tree.api"
---
---             -- 打开（关闭）目录树
---             local tree_toggle = function()
---                 api.tree.toggle()
---             end
---             vim.keymap.set('n', '<leader>e', tree_toggle, {})
---         end,
---     }
--- }
+local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
 
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
 
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    -- vim.keymap.set('n', '<C-u>', api.tree.change_root_to_parent,        opts('Up'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
 
 return {
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
+        "nvim-tree/nvim-tree.lua",
         enabled = true,
+        version = "*",
+        lazy = false,
         dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            "nvim-tree/nvim-web-devicons",
         },
-        keys = {
-            { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Open the neo-tree", mode = { "n", "v" } }
-        },
-        config = function()
-            -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-            vim.fn.sign_define("DiagnosticSignError",
-                { text = " ", texthl = "DiagnosticSignError" })
-            vim.fn.sign_define("DiagnosticSignWarn",
-                { text = " ", texthl = "DiagnosticSignWarn" })
-            vim.fn.sign_define("DiagnosticSignInfo",
-                { text = " ", texthl = "DiagnosticSignInfo" })
-            vim.fn.sign_define("DiagnosticSignHint",
-                { text = "󰌵", texthl = "DiagnosticSignHint" })
-            require("neo-tree").setup({
-                close_if_last_window = true,
-                popup_border_style = "rounded",
-                enable_git_status = true,
-                enable_diagnostics = true,
-                open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
 
-                default_component_configs = {
-                    indent_size = 2,
-                    padding = 1,
-                    -- indent guides
-                    with_markers = true,
-                    indent_marker = "│",
-                    last_indent_marker = "└",
-                    highlight = "NeoTreeIndentMarker",
-                    -- expander config, needed for nesting files
-                    with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-                    expander_collapsed = "",
-                    expander_expanded = "",
-                    expander_highlight = "NeoTreeExpander",
-                    modified = {
-                        symbol = "[+]",
-                        highlight = "NeoTreeModified",
-                    },
-                    git_status = {
-                        enabled = false,
-                        -- symbols = {
-                        --     -- Change type
-                        --     added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                        --     modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                        --     deleted   = "✖", -- this can only be used in the git_status source
-                        --     renamed   = "󰁕", -- this can only be used in the git_status source
-                        --     -- Status type
-                        --     untracked = "",
-                        --     ignored   = "",
-                        --     unstaged  = "󰄱",
-                        --     staged    = "",
-                        --     conflict  = "",
-                        -- }
+        config = function()
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+
+            local glyphs = {}
+            local installed, circles = pcall(require, 'circles')
+            if installed then
+                circles.setup({ icons = { empty = '', filled = '', lsp_prefix = '' } })
+                glyphs = circles.get_nvimtree_glyphs()
+            end
+            glyphs.git = {
+                unstaged = '',
+                staged = '',
+                unmerged = '',
+                renamed = '',
+                untracked = '',
+                deleted = '',
+                ignored = '',
+            }
+
+            require("nvim-tree").setup({
+                on_attach = my_on_attach,
+                filters = {
+                    dotfiles = true,
+                },
+                diagnostics = { enable = true },
+                view = {
+                    width = 30,
+                    side = 'left',
+                    signcolumn = 'no',
+                },
+                git = {
+                    enable = true,
+                    ignore = false,
+                    timeout = 500,
+                },
+                actions = {
+                    open_file = {
+                        -- quit_on_open = true,
                     },
                 },
-                window = {
-                    position = "left",
-                    width = 35,
-                    mapping_options = {
-                        noremap = true,
-                        nowait = true,
+                renderer = {
+                    group_empty = true,
+                    highlight_opened_files = 'all',
+                    special_files = {},
+                    root_folder_modifier = ':p:~',
+                    icons = {
+                        glyphs = glyphs,
                     },
                 },
             })
-        end
+
+            local api = require "nvim-tree.api"
+
+            -- 打开（关闭）目录树
+            local tree_toggle = function()
+                api.tree.toggle()
+            end
+            vim.keymap.set('n', '<leader>e', tree_toggle, {})
+        end,
     }
 }
+
+--[[
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		enabled = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Open the neo-tree", mode = { "n", "v" } },
+		},
+		config = function()
+			-- If you want icons for diagnostic errors, you'll need to define them somewhere:
+			vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+			vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+			vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+			vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+				open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+
+				default_component_configs = {
+					indent_size = 2,
+					padding = 1,
+					-- indent guides
+					with_markers = true,
+					indent_marker = "│",
+					last_indent_marker = "└",
+					highlight = "NeoTreeIndentMarker",
+					-- expander config, needed for nesting files
+					with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+					modified = {
+						symbol = "[+]",
+						highlight = "NeoTreeModified",
+					},
+					git_status = {
+						enabled = false,
+					},
+				},
+				window = {
+					position = "left",
+					width = 35,
+					mapping_options = {
+						noremap = true,
+						nowait = true,
+					},
+				},
+			})
+		end,
+	},
+}
+]]
--- lua/plugins/extra_ui.lua
+++ lua/plugins/extra_ui.lua
@@ -1,176 +1,122 @@
--- return {
---     {
---         'projekt0n/github-nvim-theme',
---         enabled = true,
---         tag = 'v1.0.2',
---         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
---         priority = 1000, -- make sure to load this before all the other start plugins
---         config = function()
---             require('github-theme').setup({
---                 options = {
---                     transparent = false,
---                     styles = {
---                         comments = 'italic',
---                     },
---                 },
---             })
---
---             -- local specs = require('github-theme.spec').load()
---             -- local spec = require('github-theme.spec').load('github_dark')
---             -- print(vim.inspect(spec.syntax))
---
---             vim.cmd('colorscheme github_dark')
---         end,
---     },
--- }
-
--- return {
---     "loctvl842/monokai-pro.nvim",
---     config = function()
---         require("monokai-pro").setup({})
---         vim.cmd('colorscheme monokai-pro-machine')
---     end
--- }
-
--- return {
---     {
---         "tanvirtin/monokai.nvim",
---         enabled = true,
---         config = function()
---             -- require("monokai-pro").setup({})
---             vim.cmd('colorscheme monokai_pro')
---         end
---     },
--- }
-
--- return -- Lazy
--- {
---     "olimorris/onedarkpro.nvim",
---     priority = 1000, -- Ensure it loads first
---     config = function()
---         -- somewhere in your config:
---         vim.cmd("colorscheme onedark")
---     end
--- }
-
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "amacchiato", -- latte, frappe, macchiato, mocha, auto
-            background = { -- :h background
-                light = "latte",
-                dark = "macchiato",
-            },
-            transparent_background = false, -- disables setting the background color.
-            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-            term_colors = false,   -- sets terminal colors (e.g. `g:terminal_color_0`)
-            dim_inactive = {
-                enabled = false,   -- dims the background color of inactive window
-                shade = "dark",
-                percentage = 0.15, -- percentage of the shade to apply to the inactive window
-            },
-            no_italic = false,     -- Force no italic
-            no_bold = false,       -- Force no bold
-            no_underline = false,  -- Force no underline
-            styles = {             -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" }, -- Change the style of comments
-                conditionals = { "italic" },
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-                operators = {},
-                -- miscs = {}, -- Uncomment to turn off hard-coded styles
-            },
-            custom_highlights = {},
-            default_integrations = true,
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                treesitter = true,
-                notify = false,
-                mini = {
-                    enabled = true,
-                    indentscope_color = "",
-                },
-                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-                -- telescope = {
-                --     enabled = true,
-                --     style = "nvchad",
-                -- },
-            },
-            -- config from community
-            highlight_overrides = {
-                all = function(colors)
-                    return {
-                        CurSearch = { bg = colors.sky },
-                        IncSearch = { bg = colors.sky },
-                        CursorLineNr = { fg = colors.blue, style = { "bold" } },
-                        DashboardFooter = { fg = colors.overlay0 },
-                        TreesitterContextBottom = { style = {} },
-                        WinSeparator = { fg = colors.overlay0, style = { "bold" } },
-                        ["@markup.italic"] = { fg = colors.blue, style = { "italic" } },
-                        ["@markup.strong"] = { fg = colors.blue, style = { "bold" } },
-                        Headline = { style = { "bold" } },
-                        Headline1 = { fg = colors.blue, style = { "bold" } },
-                        Headline2 = { fg = colors.pink, style = { "bold" } },
-                        Headline3 = { fg = colors.lavender, style = { "bold" } },
-                        Headline4 = { fg = colors.green, style = { "bold" } },
-                        Headline5 = { fg = colors.peach, style = { "bold" } },
-                        Headline6 = { fg = colors.flamingo, style = { "bold" } },
-                        rainbow1 = { fg = colors.blue, style = { "bold" } },
-                        rainbow2 = { fg = colors.pink, style = { "bold" } },
-                        rainbow3 = { fg = colors.lavender, style = { "bold" } },
-                        rainbow4 = { fg = colors.green, style = { "bold" } },
-                        rainbow5 = { fg = colors.peach, style = { "bold" } },
-                        rainbow6 = { fg = colors.flamingo, style = { "bold" } },
-                    }
-                end,
-            },
-            color_overrides = {
-                macchiato = {
-                    rosewater = "#F5B8AB",
-                    flamingo = "#F29D9D",
-                    pink = "#AD6FF7",
-                    mauve = "#FF8F40",
-                    red = "#E66767",
-                    maroon = "#EB788B",
-                    peach = "#FAB770",
-                    yellow = "#FACA64",
-                    green = "#70CF67",
-                    teal = "#4CD4BD",
-                    sky = "#61BDFF",
-                    sapphire = "#4BA8FA",
-                    blue = "#00BFFF",
-                    lavender = "#00BBCC",
-                    text = "#C1C9E6",
-                    subtext1 = "#A3AAC2",
-                    subtext0 = "#8E94AB",
-                    overlay2 = "#7D8296",
-                    overlay1 = "#676B80",
-                    overlay0 = "#464957",
-                    surface2 = "#3A3D4A",
-                    surface1 = "#2F313D",
-                    surface0 = "#1D1E29",
-                    -- base = "#0b0b12",
-                    base = "#262626",
-                    mantle = "#11111a",
-                    crust = "#191926",
-                },
-            },
-        })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha, auto
+			background = { -- :h background
+				light = "latte",
+				dark = "macchiato",
+			},
+			transparent_background = false, -- disables setting the background color.
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			custom_highlights = {},
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				-- telescope = {
+				--     enabled = true,
+				--     style = "nvchad",
+				-- },
+			},
+			color_overrides = {
+				macchiato = {
+					rosewater = "#F5B8AB",
+					flamingo = "#F29D9D",
+					pink = "#AD6FF7",
+					mauve = "#FF8F40",
+					red = "#E66767",
+					maroon = "#EB788B",
+					peach = "#FAB770",
+					yellow = "#FACA64",
+					green = "#70CF67",
+					teal = "#4CD4BD",
+					sky = "#61BDFF",
+					sapphire = "#4BA8FA",
+					blue = "#00BFFF",
+					lavender = "#00BBCC",
+					text = "#C1C9E6",
+					subtext1 = "#A3AAC2",
+					subtext0 = "#8E94AB",
+					overlay2 = "#7D8296",
+					overlay1 = "#676B80",
+					overlay0 = "#464957",
+					surface2 = "#3A3D4A",
+					surface1 = "#2F313D",
+					surface0 = "#1D1E29",
+					-- base = "#0b0b12",
+					base = "#262626",
+					mantle = "#11111a",
+					crust = "#191926",
+				},
+			},
 
-        -- setup must be called before loading
-        -- vim.cmd.colorscheme "catppuccin"
-        vim.cmd.colorscheme("catppuccin-macchiato")
-    end,
+			-- config from community
+			highlight_overrides = {
+				all = function(colors)
+					return {
+						CurSearch = { bg = colors.sky },
+						IncSearch = { bg = colors.sky },
+						CursorLineNr = { fg = colors.blue, style = { "bold" } },
+						DashboardFooter = { fg = colors.overlay0 },
+						TreesitterContextBottom = { style = {} },
+						WinSeparator = { fg = colors.overlay0, style = { "bold" } },
+						["@markup.italic"] = { fg = colors.blue, style = { "italic" } },
+						["@markup.strong"] = { fg = colors.blue, style = { "bold" } },
+						Headline = { style = { "bold" } },
+						Headline1 = { fg = colors.blue, style = { "bold" } },
+						Headline2 = { fg = colors.pink, style = { "bold" } },
+						Headline3 = { fg = colors.lavender, style = { "bold" } },
+						Headline4 = { fg = colors.green, style = { "bold" } },
+						Headline5 = { fg = colors.peach, style = { "bold" } },
+						Headline6 = { fg = colors.flamingo, style = { "bold" } },
+						rainbow1 = { fg = colors.blue, style = { "bold" } },
+						rainbow2 = { fg = colors.pink, style = { "bold" } },
+						rainbow3 = { fg = colors.lavender, style = { "bold" } },
+						rainbow4 = { fg = colors.green, style = { "bold" } },
+						rainbow5 = { fg = colors.peach, style = { "bold" } },
+						rainbow6 = { fg = colors.flamingo, style = { "bold" } },
+					}
+				end,
+			},
+		})
+
+		-- setup must be called before loading
+		-- vim.cmd.colorscheme "catppuccin"
+		vim.cmd.colorscheme("catppuccin-macchiato")
+	end,
 }

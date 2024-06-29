4dccd842ba17795bab6a2a26f3ca277646c19d81
Refantasy-debian <309741309@qq.com>
2024-06-29 21:01:32 +0800

    update

--- lua/plugins/core_none-ls.lua
+++ lua/plugins/core_none-ls.lua
@@ -1,20 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua.with({}),
-				null_ls.builtins.formatting.black.with({
-					extra_args = { "--line-length", 128 },
-				}),
-				null_ls.builtins.formatting.clang_format.with({
-					extra_args = { "--style", "Microsoft" },
-				}),
-			},
-		})
-	end,
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua.with({}),
+                null_ls.builtins.formatting.black.with({
+                    extra_args = { "--line-length", 128 },
+                }),
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "--style", "Microsoft" },
+                }),
+            },
+        })
+    end,
 }

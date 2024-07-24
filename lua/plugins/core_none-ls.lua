4d1c3e6dcf9733d343ad1dd6819fb5ed28a4962c
ReFantasy <309741309@qq.com>
2024-07-24 22:49:04 +0800

    update cmake formatter

--- lua/plugins/core_none-ls.lua
+++ lua/plugins/core_none-ls.lua
@@ -1,23 +1,23 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua.with({}),
-                null_ls.builtins.formatting.black.with({
-                    extra_args = { "--line-length", 128 },
-                }),
-                null_ls.builtins.formatting.clang_format.with({
-                    extra_args = { "--style", "Microsoft" },
-                }),
-                null_ls.builtins.formatting.cmake_format.with({
-                    extra_args = { "--tab-size", "4" },
-                }),
-            },
-        })
-    end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua.with({}),
+				null_ls.builtins.formatting.black.with({
+					extra_args = { "--line-length", 128 },
+				}),
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = { "--style", "Microsoft" },
+				}),
+				null_ls.builtins.formatting.cmake_format.with({
+					extra_args = { "--tab-size", "4", "--line-width", "120" },
+				}),
+			},
+		})
+	end,
 }

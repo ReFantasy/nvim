59df231f7e189e8a7b69a729d9c8cbfe2d7c45d7
Refantasy-debian <309741309@qq.com>
2024-08-01 10:46:41 +0800

    update none-ls clang-format ruler

--- lua/plugins/core_none-ls.lua
+++ lua/plugins/core_none-ls.lua
@@ -12,7 +12,7 @@ return {
 					extra_args = { "--line-length", 128 },
 				}),
 				null_ls.builtins.formatting.clang_format.with({
-					extra_args = { "--style", "Microsoft" },
+					extra_args = { "--style", "Microsoft", "-style", "\" {AccessModifierOffset:-4} \"" },
 				}),
 				null_ls.builtins.formatting.cmake_format.with({
 					extra_args = { "--tab-size", "4", "--line-width", "120" },

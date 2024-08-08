99df44bf4c08b6487ea7ee507364ff2ee61a9f6f
Refantasy-debian <309741309@qq.com>
2024-08-08 13:24:08 +0800

    update neocmakelsp

--- lua/plugins/core_lsp.lua
+++ lua/plugins/core_lsp.lua
@@ -34,6 +34,23 @@ local servers = {
 		},
 	},
 	-- cmake = {},
+	neocmake = {
+		cmd = { "neocmakelsp", "--stdio" },
+		filetypes = { "cmake" },
+		-- root_dir = function(fname)
+		--     return nvim_lsp.util.find_git_ancestor(fname)
+		-- end,
+		single_file_support = true, -- suggested
+		init_options = {
+			format = {
+				enable = true,
+			},
+			lint = {
+				enable = true,
+			},
+			scan_cmake_in_package = true, -- default is true
+		},
+	},
 }
 
 -- 映射

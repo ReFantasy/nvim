2a020a28f6e3c9b76b505a8c42b2c44eef501b90
Refantasy-debian <309741309@qq.com>
2024-10-16 17:35:33 +0800

    .

--- lua/plugins/extra_utility.lua
+++ lua/plugins/extra_utility.lua
@@ -212,6 +212,7 @@ return {
 	},
 	{
 		"karb94/neoscroll.nvim",
+        enabled = false,
 		config = function()
 			require("neoscroll").setup({
 				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" },

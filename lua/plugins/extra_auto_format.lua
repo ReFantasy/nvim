c5b192655b76594c475d9d59320773933cecc283
TanDL <refantasy@TanDLdeMac-mini.local>
2024-12-03 22:28:22 +0800

    update

--- lua/plugins/extra_auto_format.lua
+++ lua/plugins/extra_auto_format.lua
@@ -2,6 +2,7 @@ return {
 	{ -- Autoformat
 		"stevearc/conform.nvim",
 		cmd = { "ConformInfo" },
+
 		keys = {
 			{
 				"<leader>fm",
@@ -33,7 +34,8 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
+				python = { "isort", "black" },
+				cpp = {"clang-format"},
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -41,3 +43,4 @@ return {
 		},
 	},
 }
+

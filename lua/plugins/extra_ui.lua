bb73768d8d1395663b405c0ead97c16f66224a45
TanDL <refantasy@TanDLdeMac-mini.local>
2024-12-11 13:28:22 +0800

    update

--- lua/plugins/extra_ui.lua
+++ lua/plugins/extra_ui.lua
@@ -383,4 +383,17 @@ return {
 			vim.cmd.colorscheme("vscode")
 		end,
 	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<c-\>]],
+				direction = "float", --'vertical' | 'horizontal' | 'tab' | 'float',
+				float_opts = {
+					border = 'rounded',
+				},
+			})
+		end,
+	},
 }

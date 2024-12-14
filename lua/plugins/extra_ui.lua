6bf356cf0001129310f55cfe1d73bdf7847727f2
TanDL <refantasy@TanDLdeMac-mini.local>
2024-12-14 18:56:55 +0800

    update ui

--- lua/plugins/extra_ui.lua
+++ lua/plugins/extra_ui.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"catppuccin/nvim",
-		enabled = true,
+		enabled = false,
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
@@ -381,6 +381,15 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("vscode")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd.colorscheme("tokyonight-moon")
 		end,
 	},
 }

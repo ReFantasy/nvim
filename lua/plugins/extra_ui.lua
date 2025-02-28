43e23059981729ad91f5b3e1d6e7c18c77f36007
refantasy <refantasy@refantasydeMac-mini.local>
2025-02-28 16:02:43 +0800

    update

--- lua/plugins/extra_ui.lua
+++ lua/plugins/extra_ui.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"catppuccin/nvim",
-		enabled = false,
+		enabled = true,
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
@@ -385,6 +385,7 @@ return {
 	},
 	{
 		"folke/tokyonight.nvim",
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		opts = {},

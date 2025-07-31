b5e930b3b8ca25276b9fb32de79b4d6f60c0d2db
refantasy <refantasy@refantasydeMac-mini.local>
2025-07-31 10:43:33 +0800

    LazyVim

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
-vim.loader.enable()
-
-require("basic")
-require("lazy_nvim")
-
--- my plugin
--- vim.opt.rtp:prepend("$HOME/code/neovim_project/example/")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ lua/plugins/override.lua
@@ -0,0 +1,82 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   keys = {
+  --     { "<leader>l", mode = { "n" }, false },
+  --   },
+  -- },
+  {
+    "saghen/blink.cmp",
+    opts = { keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "fallback" },
+    } },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+      dashboard = {
+        preset = {
+          header = [[
+              ▀████▀▄▄              ▄█
+                █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█
+             ▄        █          ▀▀▀▀▄  ▄▀
+            ▄▀ ▀▄      ▀▄              ▀▄▀
+           ▄▀    █     █▀   ▄█▀▄      ▄█
+           ▀▄     ▀▄  █     ▀██▀     ██▄█
+            ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █
+             █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀
+            █   █  █      ▄▄           ▄▀
+            ]],
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    keys = {
+      -- disable the keymap to grep files
+      -- { "<leader>cf", mode = { "n" }, false },
+
+      -- add a keymap to browse plugin files
+      {
+        "<leader>fm",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        desc = "Format File",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "cpp" },
+      textobjects = {
+        lsp_interop = {
+          enable = true,
+          -- border = "single",
+          floating_preview_opts = {
+            border = "rounded",
+          },
+          peek_definition_code = {
+            -- ["gk"] = "@function.outer",
+            -- ["<leader>dF"] = "@class.outer",
+            ["gk"] = {
+              query = "@function.outer",
+              desc = "[Treesitter] Peek definition code (function). Press the shortcut twice to enter the floating window.",
+            },
+            ["gK"] = {
+              query = "@class.outer",
+              desc = "[Treesitter] Peek definition code (class). Press the shortcut twice to enter the floating window.",
+            },
+          },
+        },
+      },
+    },
+  },
+}

0e4ea2d5ffaafefb136dc684946dba39f15cae39
refantasy <refantasy@refantasydeMac-mini.local>
2025-08-10 20:45:00 +0800

    update

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -80,16 +80,13 @@ return {
       },
     },
   },
-
-  -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        -- pyright = {},
+        -- will be automatically installed with mason and loaded with lspconfig
         basedpyright = {
           settings = {
             basedpyright = {

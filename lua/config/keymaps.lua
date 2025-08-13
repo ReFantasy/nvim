2f1fe3c763fd39f51d68e4796db94091ebe0e1e4
ReFantasy <309741309@qq.com>
2025-08-13 10:42:38 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,16 @@
 
 vim.keymap.del({ "n" }, "<leader>l")
 vim.keymap.del({ "n" }, "<leader>cf")
+
+-- 调整窗口大小
+local opts = {
+  noremap = true, -- non-recursive
+  silent = true, -- do not show message
+}
+vim.keymap.set("n", "<S-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<S-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<S-Right>", ":vertical resize +2<CR>", opts)
+-- 分屏
+vim.keymap.set("n", "|", "<cmd>vsplit<CR>", opts)
+vim.keymap.set("n", "\\", "<cmd>split<CR>", opts)
--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -6,6 +6,15 @@ return {
     version = "2.*",
     config = function()
       require("window-picker").setup()
+
+      -- 选择窗口
+      local pick_win = function()
+        local window_number = require("window-picker").pick_window()
+        if window_number then
+          vim.api.nvim_set_current_win(window_number)
+        end
+      end
+      vim.keymap.set("n", "<leader>pw", pick_win, { desc = "[C] Pick Window" })
     end,
   },
 }

4d9211cb8fa2a354cf6c5b440a3b953adbe744fa
ReFantasy <309741309@qq.com>
2025-08-14 15:24:00 +0800

    updte

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -123,6 +123,33 @@ return {
     opts = {
       close_if_last_window = true,
       enable_git_status = true,
+      default_component_configs = {
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "󰉖",
+          folder_empty_open = "󰷏",
+          -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+          -- then these will never be used.
+          default = "*",
+          highlight = "NeoTreeFileIcon",
+          provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
+            if node.type == "file" or node.type == "terminal" then
+              local success, web_devicons = pcall(require, "nvim-web-devicons")
+              local name = node.type == "terminal" and "terminal" or node.name
+              if success then
+                local devicon, hl = web_devicons.get_icon(name)
+                icon.text = devicon or icon.text
+                icon.highlight = hl or icon.highlight
+              end
+            end
+          end,
+        },
+      },
+      window = {
+        position = "left",
+        width = 35,
+      },
     },
   },
   {

f8823736d831245194eb3931e28654a550b582a6
Refantasy-debian <309741309@qq.com>
2025-09-21 18:22:39 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "telescope"
+
 vim.g.snacks_animate = false
 vim.o.winborder = "rounded"
 vim.g.lazyvim_picker = "fzf"

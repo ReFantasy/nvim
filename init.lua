959acab986ef14df97767f7c15c0642e60243d1a
refantasy <refantasy@refantasydeMac-mini.local>
2025-10-05 16:34:25 +0800

    update

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require("basic")
 require("lsp_config")
+
 require("plugins.mason")
 require("plugins.blink")
 
+-- vim.api.nvim_command("colorscheme default")
--- lua/basic.lua
+++ lua/basic.lua
@@ -1,18 +1,12 @@
 vim.g.mapleader = " "
+vim.o.winborder = 'rounded' -- rounded windows 
 
-vim.o.winborder = 'rounded'
-
--- 显示行号
 vim.opt.number = true -- show absolute number
-vim.opt.relativenumber = true
--- 高亮光标行
-vim.opt.cursorline = true 
--- 默认新创建窗口在右和下位置
-vim.opt.splitbelow = true 
-vim.opt.splitright = true 
--- 外观
+-- vim.opt.relativenumber = true
+vim.opt.cursorline = true -- 高亮光标行
 vim.opt.termguicolors = true -- enabl 24-bit RGB color in the TUI
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "no" -- yes or no
+
 -- 分屏
 vim.keymap.set("n", "|", "<cmd>vsplit<CR>")
 vim.keymap.set("n", "\\", "<cmd>split<CR>")

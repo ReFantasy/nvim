6b43dfbb6de0f07e3089d35f9ca990babd1ab1a7
refantasy <refantasy@refantasydeMac-mini.local>
2025-10-08 13:29:37 +0800

    update

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 require("basic")
 
--- language server
+-- enable language server
 vim.lsp.enable 'lua_ls'
 vim.lsp.enable 'clangd'
 
--- plugins
-require("plugins.mason")
-require("plugins.blink")
---require("plugins.neo-tree")
+-- plugins manager
+require("plugins.mason") 
+require("plugins.blink") 
+require("plugins.neo-tree")
+
 
 -- vim.api.nvim_command("colorscheme default")
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,15 @@
 vim.pack.add({
-    {
-        src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
-        version = vim.version.range('3')
-    },
-    -- dependencies
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    -- optional, but recommended
-    "nvim-tree/nvim-web-devicons",
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range('3')
+  },
+  -- dependencies
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  -- optional, but recommended
+  "https://github.com/nvim-tree/nvim-web-devicons",
 })
 require('neo-tree').setup({
-    -- options go here
+  -- options go here
 })
+

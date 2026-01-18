a78130f273c3b9b1bdb04763b83926826d1598dd
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-01-18 13:47:01 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 禁止动画效果
+vim.g.snacks_animate = false
+-- 全局圆角边框
+vim.o.winborder = "rounded"
+
+-- Tab
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+vim.opt.autoindent = true

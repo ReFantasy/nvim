c6ecced429b91a02fa6ee8471e0567e26ad72bfd
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-01-20 12:57:27 +0800

    update cmdline position

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,11 +1,11 @@
 return {
-    {
-        "folke/snacks.nvim",
-        opts = {
-            indent = { enabled = false },
-            dashboard = {
-                preset = {
-                    header = [[
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+      dashboard = {
+        preset = {
+          header = [[
               ▀████▀▄▄              ▄█
                 █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█
              ▄        █          ▀▀▀▀▄  ▄▀
@@ -16,40 +16,48 @@ return {
              █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀
             █   █  █      ▄▄           ▄▀
             ]],
-                },
+        },
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+            layout = {
+              preset = "sidebar",
+              preview = false,
+              auto_hide = { "input" },
             },
-            picker = {
-                sources = {
-                    explorer = {
-                        -- your explorer picker configuration comes here
-                        -- or leave it empty to use the default settings
-                        layout = {
-                            preset = "sidebar",
-                            preview = false,
-                            auto_hide = { "input" },
-                        },
-                    }
-                }
-            }
+          },
         },
+      },
     },
-    {
-        "saghen/blink.cmp",
-        -- use a release tag to download pre-built binaries
-        version = "1.*",
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-        opts = {
-            keymap = {
-                preset = "enter",
-                ["<Tab>"] = { "select_next", "fallback" },
-            }
-        },
+  },
+  {
+    "saghen/blink.cmp",
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = { "select_next", "fallback" },
+      },
     },
-    {
-        "akinsho/bufferline.nvim",
-        enabled = false,
-    }
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
 }

8c284200017bfe2ac0fcbf1214efb71fae49d21d
ReFantasy <309741309@qq.com>
2026-01-20 13:33:11 +0800

    update

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -58,6 +58,12 @@ return {
       cmdline = {
         view = "cmdline",
       },
+      -- lsp = {
+      --   override = {
+      --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      --     ["vim.lsp.util.stylize_markdown"] = true,
+      --   },
+      -- },
     },
   },
 }

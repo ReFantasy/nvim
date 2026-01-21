8896ac8c9223b6edd84753649cf0f2831a66530a
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-01-21 10:07:00 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,4 +20,5 @@ vim.keymap.set("n", "\\", "<cmd>split<CR>")
 --   print("okkkkkkkkkkkkkkkkkk")
 -- end)
 
-vim.keymap.set({ "n", "i", "v" }, "<leader>qa", "<cmd>wa<CR><cmd>qa<CR>", { desc = "save all and quit vim" })
+-- vim.keymap.set({ "n", "i", "v" }, "<leader>qa", "<cmd>wa<CR><cmd>qa<CR>", { desc = "save all and quit vim" })
+vim.keymap.set({ "n", "i", "v" }, "<leader>qq", "<cmd>wa<CR><cmd>qa<CR>", { desc = "save all and quit vim" })

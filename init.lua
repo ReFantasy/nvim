a0ea4e39d798fe37bebe0344fe0ad971be46d707
ReFantasy <refantasy@192.168.0.105>
2026-02-05 18:44:18 +0800

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
+require("config.options")
+
 -- do :Lazy restore, to update all your plugins to the version from the lockfile.
 require("config.lazy")
---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- 全局圆角边框
+vim.o.winborder = "rounded"
+
+-- Tab
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+vim.opt.autoindent = true
+
+
+-- vim.cmd("colorscheme desert")

76f023e63579535160f007e75b4cd911502cbf9e
ReFantasy <309741309@qq.com>
2026-02-05 11:06:10 +0800

    update

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -37,5 +37,5 @@ return {
       },
     },
   },
-  -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
+  { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }

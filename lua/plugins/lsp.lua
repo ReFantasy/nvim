a6774986c5d51d56b5833bcd63b7468e97495938
ReFantasy <309741309@qq.com>
2026-02-07 16:18:51 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,7 +130,7 @@ return {
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
 				"clang-format",
-				"black",
+				-- "black",
 			})
 
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })

a91d88d08f2a044ee196736e0f65cc15dd5b5379
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-03-10 09:07:27 +0800

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -38,5 +38,10 @@ return { -- Autoformat
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
+		formatters = {
+			black = {
+				prepend_args = { "--line-length", "120" },
+			},
+		},
 	},
 }

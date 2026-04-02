0bab0b2e0f0cff72b0d01be82544af16785d1f7a
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-04-02 09:34:43 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,24 @@ vim.keymap.set("n", "\\", "<cmd>split<CR>")
 -- vim.keymap.set("v", "H", "<gv", opts)
 -- vim.keymap.set("v", "L", ">gv", opts)
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(event)
+		-- In this case, we create a function that lets us more easily define mappings specific
+		-- for LSP related items. It sets the mode, buffer and description for us each time.
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
+		-- 定义快捷键
+
+		-- map("ga", vim.lsp.buf.code_action, "[L]sp Code Action", { "n", "x" })
+		-- map("gn", vim.lsp.buf.rename, "[L]sp Code Rename", { "n", "x" })
+		-- map("gD", vim.lsp.buf.declaration, "[L]sp [D]eclaration")
+
+		if client and client:supports_method("textDocument/inlayHint", event.buf) then
+			map("<leader>th", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+			end, "[T]oggle Inlay [H]ints")
+		end
+	end,
+})

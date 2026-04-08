1c7ecfe36c2804e0e91322e18e477a01f995bafa
ReFantasy <309741309@qq.com>
2026-04-08 21:58:51 +0800

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,6 @@ vim.pack.add({
 require("nvim-treesitter").setup({
 	highlight = { enable = true }, -- 启用高亮（推荐）
 	indent = { enable = true }, -- 启用智能缩进（推荐）
-	folding = { enable = true }, -- **启用折叠**
 })
 
 -- 折叠配置 (Folding) ==
@@ -13,11 +12,15 @@ require("nvim-treesitter").setup({
 -- zM：关闭文件中所有折叠。
 -- zR：打开文件中所有折叠。
 -- zi：切换折叠功能 ('foldenable') 的启用/禁用状态。
-vim.opt.foldenable = true -- 启用折叠功能
-vim.opt.foldmethod = "expr" -- 使用表达式进行折叠
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- 指定折叠表达式为 Tree-sitter 函数
-vim.opt.foldlevel = 99 -- 默认打开所有折叠（推荐：文件打开时全部展开）
-vim.opt.foldcolumn = "1" -- 在左侧显示一列来表示折叠（可选，设为"0"则不显示）
+vim.wo.foldenable = true -- 启用折叠功能
+vim.wo.foldcolumn = "1" -- 在左侧显示一列来表示折叠（可选，设为"0"则不显示）
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- 控制最小折叠行数（避免单行被折叠）
+vim.wo.foldminlines = 1
+vim.wo.foldlevel = 99 -- 默认打开所有折叠（推荐：文件打开时全部展开）
+-- 最大折叠嵌套层级
+vim.wo.foldnestmax = 5
 
 local filetypes = {
 	"c",
